2140265e5237eaa15d958d58bf369caaa7c79fe1
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-01-05 22:58:43 +0530

    JS
    JS file added

---
+++ frontend-todo-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="heading1">ToDo App</h1>
+      <div>
+        <h3>Hi there👋</h3>
+      </div>
+      <div>
+        <a href="https://github.com/ayushmanyd" target="_blank">
+          GitHub
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
