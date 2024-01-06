014173c875b093342d78b687a0483755c6ee852a
Ayushman Pratap Yadav <155382910+ayushmanyd@users.noreply.github.com>
2024-01-06 23:54:33 +0530

    Update App.jsx
    Update content dynamically.

--- frontend-todo-app/src/App.jsx
+++ frontend-todo-app/src/App.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "College",
+      description: "In college from 8 to 2.",
+    },
+    {
+      id: 2,
+      title: "Meeting",
+      description: "Attend meeting from 5 to 6.",
+    },
+    {
+      id: 3,
+      title: "Coding",
+      description: "Code till 11.",
+    },
+  ]);
 
   return (
     <>
@@ -11,11 +26,23 @@ function App() {
       <div>
         <h3>Hi there👋</h3>
       </div>
+
+      {todo.map((todo) => {
+        return (
+          <div>
+            {todo.title}
+            {todo.description}
+            <br />
+          </div>
+        );
+      })}
+
       <div>
         <a href="https://github.com/ayushmanyd" target="_blank">
           GitHub
         </a>
       </div>
+
     </>
   );
 }
